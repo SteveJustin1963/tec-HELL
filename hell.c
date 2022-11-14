debad1146c9ce529af9a50aa6284ea6679264e0c
Stephen Justin <stevejustin1963@gmail.com>
2022-11-14 20:32:08 +1100

    Update hell.c

--- hell.c
+++ hell.c
@@ -1,48 +1,25 @@
 
 #include <stdio.h>
-
 int main()
-
 {
-
 int i, j;
-
 char input[100];
-
 printf("Enter a message to be sent using Hellschreiber:
-
 ");
-
 scanf("%s", input);
-
 for (i = 0; input[i] != '\0'; i++)
-
 {
-
 for (j = 7; j >= 0; j--)
-
 {
-
 if (input[i] & (1 << j))
-
 {
-
 printf("-.-");
-
 }
-
 else
-
 {
-
 printf(".-.");
-
 }
-
 }
-
 }
-
 return 0;
-
 }
